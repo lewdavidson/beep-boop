0115e51b22adf611582a9750f4ca76652353a883
Lew Davidson <lewdavidson47@gmail.com>
2017-10-27 15:50:54 -0700

    still not show ones as beeps and zeroes as boops

--- js/scripts.js
+++ js/scripts.js
@@ -10,50 +10,71 @@
 $(document).ready(function() {
   $("form#numberForm").submit(function(event) {
     event.preventDefault();
-    var startingNumber = parseInt($("input#number-input").val());
-    // var startingString = $("input#number-input").val();
-    // var startingArray = startingString.split();
-    var ones = [1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 31, 41, 51, 61, 71, 81, 91, 100]
-    var zeroes = [10, 20, 30, 40, 50, 60, 70, 80, 90]
-    var userOutput = [];
+        var startingNumber = parseInt($("input#number-input").val());
+        // var startingString = $("input#number-input").val();
+        // var startingArray = startingString.split();
+        var ones = [1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21, 31, 41, 51, 61, 71, 81, 91, 100]
+        var zeroes = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+        var userOutput = [];
+        var onesOutput = [];
+        var tensOutput = [];
 
-    for (var index = 0; index <= startingNumber; index++) {
-       userOutput.push(index); //basic display
-       $("#answer").show();
-       $("#answer").text(userOutput);
-     }
+        for (var index = 0; index <= startingNumber; index++) {
+           userOutput.push(index); //basic display
+           $("#answer").show();
+           $("#answer").text(userOutput);
+         }
 
-    if (startingNumber === 0) {
-      $("#answer").show();
-      $("#answer").text("beep");
-    } else if (startingNumber % 3 === 0) {
-      $("#answer").show();
-      //  userOutput.push("I'm sorry, Dave. I'm afraid I can't do that.");
-      $("#answer").text("I'm sorry, Dave. I'm afraid I can't do that.");
-    } else if (startingNumber % 10 === 0) {
-        for (var index = 0; index < userOutput.length; index++) {
-          for (var searchIndex = 0; searchIndex < zeroes.length; searchIndex++) {
-              if (userOutput[index] === zeroes[searchIndex]) {
-              userOutput[index] = "beep";
-              userOutput.push(index); //basic display
-              $("#answer").show();
-              $("#answer").text(userOutput); //zero rule
-            }
+        // if (startingNumber === 0) {
+        //   $("#answer").show();
+        //   $("#answer").text("beep");
+        // } else if (startingNumber % 3 === 0) {
+        //   $("#answer").show();
+        //   $("#answer").text("I'm sorry, Dave. I'm afraid I can't do that.");
+        // } else if {
+        //   for (var index = 0; index < userOutput; index++); {
+        //      if (user.output.charAt(0) === 0) {
+        //        var cut = userOutput.slice(0);
+        //        cut.text("beep");
+        //
+        //
+        //      }
+        //     }
+        //
+        // }
+        
+        userOutput.forEach(function(outputs){
+          if (outputs === 0) {
+            userOutput.text("beep");
+          } else if (outputs === 1) {
+            userOutput.text("boop");
           }
-        }
-      } else { for (var index = 0; index < userOutput.length; index++) {
-          for (var lookingIndex = 0; lookingIndex < ones.length; lookingIndex++) {
-            if (userOutput[index] === ones[lookingIndex]) { //one rule
-              userOutput[index] = "boop";
-              userOutput.push(index); //basic display
-              $("#answer").show();
-              $("#answer").text(userOutput);
-            }
-          }
-        }
-      }
+
+      });
+    });
 
 
+    });
+      // for (var index = 0; index < userOutput.length; index++) {
+      //   for (var lookingIndex = 0; lookingIndex < ones.length; lookingIndex++) {
+      //     if (userOutput[index] === ones[lookingIndex]) { //one rule
+      //       userOutput[index] = "boop";
+      //       userOutput.push(index);
+      //       $("#answer").show();
+      //       $("#answer").text(userOutput // broken one rule
+
+
+      // } else if (startingNumber % 10 === 0) {
+      //   for (var index = 0; index < userOutput.length; index++) {
+      //     for (var searchIndex = 0; searchIndex < zeroes.length; searchIndex++) {
+      //       if (userOutput[index] === zeroes[searchIndex]) {
+      //         userOutput[index] = "beep";
+      //         userOutput.push(index);
+      //         $("#answer").show();
+      //         $("#answer").text(userOutput); //broken zero rule
+      //       }
+      //     }
+      //   }
 
     // else {
     //   for (var index = 0; index <= startingNumber; index++) {
@@ -63,8 +84,6 @@ $(document).ready(function() {
     //    }
     //  }
 
-    });
-  });
 
 //     for (var index = 0; index < userOutput.length; index++) {
 //       for (var lookingIndex = 0; lookingIndex < ones.length; lookingIndex++) {
