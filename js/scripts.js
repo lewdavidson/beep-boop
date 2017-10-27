bd13b1ca5d2f2fb86597b9d1ca0a1d9629dc5f1c
Lew Davidson <lewdavidson47@gmail.com>
2017-10-27 11:27:27 -0700

    add nested for loop

--- js/scripts.js
+++ js/scripts.js
@@ -11,30 +11,40 @@ $(document).ready(function() {
   $("form#numberForm").submit(function(event) {
     event.preventDefault();
     var startingNumber = parseInt($("input#number-input").val());
-    var startingArray = startingNumber.split();
+    // var startingString = $("input#number-input").val();
+    // var startingArray = startingString.split();
     var ones = [1, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 21]
     var userOutput = [];
 
-    for (var index = 0; index < startingArray.length; index++) { debugger
+
+
+    for (var index = 0; index <= startingNumber; index++) {
+       userOutput.push(index);
+       $("#answer").show();
+       $("#answer").text(userOutput); 
+     }
+    for (var index = 0; index < userOutput.length; index++) { debugger
       for (var lookingIndex = 0; lookingIndex < ones.length; lookingIndex++) {
-        if (startingArray[index] === ones[lookingIndex]) {
-          startingArray[index] = ("boop"); debugger
+        if (userOutput[index] === ones[lookingIndex]) { debugger
+          userOutput.text("boop");
+          alert(userOutput);
         }
       }
     }
 
-    // var output = startingArray.join();
-    // $("#answer").show();
-    // $("#answer").append(startingNumber[index]);
 
   });
 });
 
-// for (var index = 0; index <= startingNumber; index++) {
-//    userOutput.push(index);
+
+//
+//       // var output = startingArray.join();
+//       // $("#answer").show();
+//       // $("#answer").append(startingNumber[index]);
+//     }
+//   }
 // }
-// $("#answer").show();
-// $("#answer").append(userOutput);
+
 
 
     // for (var numberIndex = 0; numberIndex < startingNumber.length; numberIndex ++) {
