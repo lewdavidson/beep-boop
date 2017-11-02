fb1dd9e77532bc98f65c453b3d266da0e5a3c4ad
Lew <lewdavidson47@gmail.com>
2017-11-01 21:28:08 -0700

    make changes to the .replace() function

--- js/scripts.js
+++ js/scripts.js
@@ -3,16 +3,16 @@ $(document).ready(function() {
 
   var userOutput = [];
 
-  for (var index = 0; index <= startingNumber; index++) {
-    userOutput.push(index);
-     //basic display
-  }
   for (var i; i <=startingNumber; index++) {
     if(userOutput[i].length > 1) {
-      userOutput[i] = userOutput[i].replace("0", "beep").replace("1", "boop");
+      userOutput[i] = userOutput[i].toString().replace("0", "beep").toString().replace("1", "boop");
+      //need regex to replace? must be a string to use .replace(), regex more thurough?
     }
   }
-
+  for (var index = 0; index <= startingNumber; index++) {
+    userOutput.push(index);
+  }
+    //basic display
 
 
 // user interface
@@ -21,11 +21,5 @@ $(document).ready(function() {
         var startingNumber = parseInt($("input#number-input").val());
         $("#answer").show();
         $("#answer").text(userOutput);
-
-
-
-
-
-
   });
 });
