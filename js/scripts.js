afd06daa5e6d86a1fdc7fb1145e05b46b9b5597c
Lew Davidson <lewdavidson47@gmail.com>
2017-11-05 11:19:01 -0800

    not work, trying to debug

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,22 @@
 // biz logic
 
+  var userOutput = [];
+  
+  for (var index = 0; index <= startingNumber; index++) {
+    userOutput.push(index);
+  }
+  //basic display
 
-var userOutput = [];
-for (var index = 0; index <= startingNumber; index++) {
-  userOutput.push(index);
-}
-//basic display
   for (var i = 0; i <= userOutput.length; index++) {
     if(userOutput[i].toString().length > 1) {
       userOutput[i] = userOutput[i].toString().replace(/0/g, "beep").toString().replace(/1/g, "boop"); //add regex global!
     } else if (userOutput[i].toString() === "0") {
-      userOutput.toString().replace("beep"); //not replace - something else.
+      userOutput[i] === "beep"; //not replace - something else.
     } else if (userOutput [i].toString() === "1") {
-      userOutput.toString().replace ("boop");
-    } else (userOutput[i] %3 === 0) {
-      userOutput.toString().replace ("I'm sorry Dave, Im afraid I can't do that.")
-    }
+      userOutput[i] === "boop";
+    } else if (userOutput[i] %3 === 0) {
+      userOutput[i] === "I'm sorry Dave, Im afraid I can't do that.";
+    } else {}
   }
 
 
