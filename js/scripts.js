43137daa74ce94f1ae95d935d24b4235e08234b7
Lew Davidson <lewdavidson47@gmail.com>
2017-11-05 11:12:47 -0800

    debugging steps?

--- js/scripts.js
+++ js/scripts.js
@@ -8,10 +8,10 @@ for (var index = 0; index <= startingNumber; index++) {
 //basic display
   for (var i = 0; i <= userOutput.length; index++) {
     if(userOutput[i].toString().length > 1) {
-      userOutput[i] = userOutput[i].toString().replace(/0/g, "beep").toString().replace(/1/g, "boop"); //add regex global for specificity
+      userOutput[i] = userOutput[i].toString().replace(/0/g, "beep").toString().replace(/1/g, "boop"); //add regex global!
     } else if (userOutput[i].toString() === "0") {
       userOutput.toString().replace("beep"); //not replace - something else.
-    } else if (userOutput [i] === "1") {
+    } else if (userOutput [i].toString() === "1") {
       userOutput.toString().replace ("boop");
     } else (userOutput[i] %3 === 0) {
       userOutput.toString().replace ("I'm sorry Dave, Im afraid I can't do that.")
