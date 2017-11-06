c8506c38b020f099818782c526efbcf137f015d5
Lew Davidson <lewdavidson47@gmail.com>
2017-11-05 21:14:59 -0800

    IT WORKSgit add .

--- js/scripts.js
+++ js/scripts.js
@@ -3,24 +3,22 @@
   var userOutput = [];
   var startingNumber = "";
 
-  if (startingNumber) {
+function calculate(startingNumber) {
   for (var index = 0; index <= startingNumber; index++) {
     userOutput.push(index);
   }
-}
-  //basic display
-
-  for (var i = 0; i <= userOutput.length; index++) {
+  for (var i = 0; i < userOutput.length; i++) {
     if(userOutput[i].toString().length > 1) {
-      userOutput[i] = userOutput[i].toString().replace(/0/g, "beep").toString().replace(/1/g, "boop"); //add regex global!
+      userOutput[i] = userOutput[i].toString().replace(/0/g, "beep").toString().replace(/1/g, "boop"); 
     } else if (userOutput[i].toString() === "0") {
-      userOutput[i] === "beep"; //not replace - something else.
+      userOutput[i] = "beep";
     } else if (userOutput [i].toString() === "1") {
-      userOutput[i] === "boop";
+      userOutput[i] = "boop";
     } else if (userOutput[i] %3 === 0) {
-      userOutput[i] === "I'm sorry Dave, Im afraid I can't do that.";
-    } else {}
+      userOutput[i] = "I'm sorry Dave, Im afraid I can't do that.";
+    } else { }
   }
+}
 
 
 // user interface
@@ -28,6 +26,7 @@ $(document).ready(function() {
   $("#number-form").submit(function(event) {
     event.preventDefault();
         var startingNumber = parseInt($("#number-input").val());
+        calculate(startingNumber);
         $("#answer").show();
         $("#answer").text(userOutput);
   });
